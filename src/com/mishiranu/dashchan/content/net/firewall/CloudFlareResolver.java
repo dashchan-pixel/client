853b8e3030e0bbd6aa7e82abd6a87cc0356beec6
nemesis <nemesis.mk47@gmail.com>
2023-02-27 09:29:32 +0300

    Remove the title check from the onPageFinished method due to possible title's localization and rewrite the rest of the code to comply with this change
    These changes are needed because the old resolution method relies on checking if the page's title is one of the titles that Cloudflare can use. But Cloudflare can use localized titles, and this breaks resolution logic. The code checks the page's title, sees that the title is not one of Cloudflare's titles because it is localized, and decides that resolution has finished. Then the code tries to collect the cookie, but the cookie is not available because the resolution has not actually finished. That leads to resolution failure.

--- src/com/mishiranu/dashchan/content/net/firewall/CloudFlareResolver.java
+++ src/com/mishiranu/dashchan/content/net/firewall/CloudFlareResolver.java
@@ -2,101 +2,69 @@ package com.mishiranu.dashchan.content.net.firewall;
 
 import android.net.Uri;
 import android.os.Parcel;
+
+import com.mishiranu.dashchan.R;
+import com.mishiranu.dashchan.content.MainApplication;
+import com.mishiranu.dashchan.content.Preferences;
+import com.mishiranu.dashchan.content.service.webview.WebViewExtra;
+import com.mishiranu.dashchan.util.IOUtils;
+
+import java.net.HttpURLConnection;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import chan.content.Chan;
 import chan.http.CookieBuilder;
 import chan.http.FirewallResolver;
 import chan.http.HttpException;
 import chan.http.HttpResponse;
 import chan.util.StringUtils;
-import com.mishiranu.dashchan.R;
-import com.mishiranu.dashchan.content.MainApplication;
-import com.mishiranu.dashchan.content.Preferences;
-import com.mishiranu.dashchan.content.service.webview.WebViewExtra;
-import com.mishiranu.dashchan.util.IOUtils;
-import java.net.HttpURLConnection;
-import java.util.Map;
 
 public class CloudFlareResolver extends FirewallResolver {
 	private static final String COOKIE_CLOUDFLARE = "cf_clearance";
-	private static final String[] TITLES = {"Attention Required! | Cloudflare", "Just a moment...", "Please wait…"};
 
-	private static class CookieResult {
-		public final String cookie;
-		public final Uri uri;
-
-		public CookieResult(String cookie, Uri uri) {
-			this.cookie = cookie;
-			this.uri = uri;
+	@Override
+	public CheckResponseResult checkResponse(Session session, HttpResponse response) throws HttpException {
+		boolean pageBlockedByCloudflare = pageBlocked(response) && (responseContainsCloudflareHeaders(response) || responseContainsCloudflareTitle(response));
+		if (pageBlockedByCloudflare) {
+			return new CheckResponseResult(toKey(session), new Exclusive());
+		} else {
+			return null;
 		}
 	}
 
-	private static class Extra implements WebViewExtra {
-		@Override
-		public String getInjectJavascript() {
-			return IOUtils.readRawResourceString(MainApplication.getInstance().getResources(),
-					R.raw.web_cloudflare_inject);
-		}
-
-		public static final Creator<Extra> CREATOR = new Creator<Extra>() {
-			@Override
-			public Extra createFromParcel(Parcel in) {
-				return new Extra();
-			}
-
-			@Override
-			public Extra[] newArray(int size) {
-				return new Extra[0];
-			}
-		};
+	private boolean pageBlocked(HttpResponse response) {
+		int responseCode = response.getResponseCode();
+		return responseCode == HttpURLConnection.HTTP_FORBIDDEN || responseCode == HttpURLConnection.HTTP_UNAVAILABLE;
 	}
 
-	private static class WebViewClient extends FirewallResolvers.WebViewClientWithExtra<CookieResult> {
-		private final String title;
-
-		public WebViewClient(String title) {
-			super("CloudFlare", new Extra());
-			this.title = title;
+	private boolean responseContainsCloudflareHeaders(HttpResponse response) {
+		Map<String, List<String>> responseHeaders = response.getHeaderFields();
+		boolean responseContainsCloudflareRayHeader = responseHeaders.containsKey("CF-RAY");
+		if (!responseContainsCloudflareRayHeader) {
+			List<String> serverHeader = responseHeaders.get("Server");
+			return serverHeader != null && serverHeader.contains("cloudflare");
 		}
+		return true;
+	}
 
-		@Override
-		public boolean onPageFinished(Uri uri, Map<String, String> cookies, String title) {
-			if (title != null && title.equals(this.title)) {
-				return false;
-			}
-			for (String checkTitle : TITLES) {
-				if (checkTitle.equals(title)) {
-					return false;
+	// Cloudflare uses localized titles in some challenges, so this method is very unreliable
+	private boolean responseContainsCloudflareTitle(HttpResponse response) throws HttpException {
+		Pattern titlePattern = Pattern.compile("<title>(.*?)</title>");
+		String responseText = response.readString();
+		Matcher titleMatcher = titlePattern.matcher(responseText);
+		if (titleMatcher.find()) {
+			String[] cloudflareTitles = {"Attention Required! | Cloudflare", "Just a moment...", "Please wait…"};
+			String title = titleMatcher.group(1);
+			for (String cloudflareTitle : cloudflareTitles) {
+				if (cloudflareTitle.equals(title)) {
+					return true;
 				}
 			}
-			String cookie = cookies.get(COOKIE_CLOUDFLARE);
-			setResult(cookie != null ? new CookieResult(cookie, uri) : null);
-			return true;
 		}
-
-		@Override
-		public boolean onLoad(Uri initialUri, Uri uri) {
-			String path = uri.getPath();
-			return path == null || path.isEmpty() || "/".equals(path) ||
-					path.equals(initialUri.getPath()) || path.startsWith("/cdn-cgi/");
-		}
-	}
-
-	private class Exclusive implements FirewallResolver.Exclusive {
-		public final String title;
-
-		public Exclusive(String title) {
-			this.title = title;
-		}
-
-		@Override
-		public boolean resolve(Session session, Key key) throws CancelException, InterruptedException {
-			CookieResult result = session.resolveWebView(new WebViewClient(title));
-			if (result != null) {
-				storeCookie(session, key, result.cookie, result.uri);
-				return true;
-			}
-			return false;
-		}
+		return false;
 	}
 
 	private Exclusive.Key toKey(Session session) {
@@ -104,37 +72,13 @@ public class CloudFlareResolver extends FirewallResolver {
 	}
 
 	@Override
-	public CheckResponseResult checkResponse(Session session, HttpResponse response) throws HttpException {
-		int responseCode = response.getResponseCode();
-		if ((responseCode == HttpURLConnection.HTTP_FORBIDDEN || responseCode == HttpURLConnection.HTTP_UNAVAILABLE)
-				&& response.getHeaderFields().containsKey("CF-RAY")) {
-			String responseText = response.readString();
-			switch (responseCode) {
-				case HttpURLConnection.HTTP_FORBIDDEN:
-				case HttpURLConnection.HTTP_UNAVAILABLE: {
-					String title = null;
-					for (String checkTitle : TITLES) {
-						if (responseText.contains("<title>" + checkTitle + "</title>")) {
-							title = checkTitle;
-						}
-					}
-					if (responseText.contains("<form class=\"challenge-form\" id=\"challenge-form\"") &&
-							(responseText.contains("__cf_chl_captcha_tk__") || responseText.contains("__cf_chl_f_tk"))) {
-						int start = responseText.indexOf("<title>");
-						if (start >= 0) {
-							int end = responseText.indexOf("</title>", start);
-							if (end > start) {
-								title = responseText.substring(start + 7, end);
-							}
-						}
-					}
-					if (title != null) {
-						return new CheckResponseResult(toKey(session), new Exclusive(title));
-					}
-				}
-			}
+	public void collectCookies(Session session, CookieBuilder cookieBuilder) {
+		Chan chan = session.getChan();
+		Exclusive.Key key = toKey(session);
+		String cookie = chan.configuration.getCookie(key.formatKey(COOKIE_CLOUDFLARE));
+		if (!StringUtils.isEmpty(cookie)) {
+			cookieBuilder.append(COOKIE_CLOUDFLARE, cookie);
 		}
-		return null;
 	}
 
 	private void storeCookie(Session session, Exclusive.Key key, String cookie, Uri uri) {
@@ -151,13 +95,70 @@ public class CloudFlareResolver extends FirewallResolver {
 		}
 	}
 
-	@Override
-	public void collectCookies(Session session, CookieBuilder cookieBuilder) {
-		Chan chan = session.getChan();
-		Exclusive.Key key = toKey(session);
-		String cookie = chan.configuration.getCookie(key.formatKey(COOKIE_CLOUDFLARE));
-		if (!StringUtils.isEmpty(cookie)) {
-			cookieBuilder.append(COOKIE_CLOUDFLARE, cookie);
+	private static class CookieResult {
+		public final String cookie;
+		public final Uri uri;
+
+		public CookieResult(String cookie, Uri uri) {
+			this.cookie = cookie;
+			this.uri = uri;
+		}
+	}
+
+	private static class Extra implements WebViewExtra {
+		public static final Creator<Extra> CREATOR = new Creator<Extra>() {
+			@Override
+			public Extra createFromParcel(Parcel in) {
+				return new Extra();
+			}
+
+			@Override
+			public Extra[] newArray(int size) {
+				return new Extra[0];
+			}
+		};
+
+		@Override
+		public String getInjectJavascript() {
+			return IOUtils.readRawResourceString(MainApplication.getInstance().getResources(),
+					R.raw.web_cloudflare_inject);
+		}
+	}
+
+	private static class WebViewClient extends FirewallResolvers.WebViewClientWithExtra<CookieResult> {
+
+		public WebViewClient() {
+			super("CloudFlare", new Extra());
+		}
+
+		@Override
+		public boolean onPageFinished(Uri uri, Map<String, String> cookies, String title) {
+			String cookie = cookies.get(COOKIE_CLOUDFLARE);
+			if (cookie == null) {
+				return false;
+			}
+			setResult(new CookieResult(cookie, uri));
+			return true;
+		}
+
+		@Override
+		public boolean onLoad(Uri initialUri, Uri uri) {
+			String path = uri.getPath();
+			return path == null || path.isEmpty() || "/".equals(path) ||
+					path.equals(initialUri.getPath()) || path.startsWith("/cdn-cgi/");
+		}
+	}
+
+	private class Exclusive implements FirewallResolver.Exclusive {
+
+		@Override
+		public boolean resolve(Session session, Key key) throws CancelException, InterruptedException {
+			CookieResult result = session.resolveWebView(new WebViewClient());
+			if (result != null) {
+				storeCookie(session, key, result.cookie, result.uri);
+				return true;
+			}
+			return false;
 		}
 	}
 }
